8122c4d667257855e36638e157a19f196288693f
Travis <bobo@petticrew.net>
2011-08-23 17:26:07 -0500

    Added Scope class to replace scope() method on Base

---
+++ pyperry/scope.py
@@ -0,0 +1,82 @@
+class Scope(object):
+    """
+    Defines a query shortcut.
+
+    A scope can be defined in one of several ways:
+
+    Dictionary or Keyword Arguments
+
+    If your scope is simply setting a few static query arguments than this
+    is the easiest option.  Here are a few examples::
+
+        # With a dictionary
+        ordered = Scope({ 'order': 'name' })
+
+        # With keyword arguments
+        awesome = Scope(where={'awesome': 1})
+        latest = Scope(order="created_at DESC", limit=1)
+
+    With a Callable (as decorators)
+
+    When your scope involves chaining other scopes, delayed values (such as
+    a relative time), or if it takes arguments then this is the preferred
+    method.  Here are a few examples::
+
+        @Scope
+        def awesome_ordered(cls):
+            return cls.ordered().awesome()
+
+        # Returns a scope dynamically generating condition using fictional
+        # minutes_ago function.  Without the lambda this wouldn't update
+        # each time the scope is used, but only when the code was reloaded.
+
+        @Scope
+        def recent(cls):
+            return cls.where('created_at > %s' % minutes_ago(5))
+
+        # You can also pass arguments at runtime!
+
+        @Scope
+        def name_like(cls, word):
+            return cls.where(["name LIKE '%?%", word])
+
+    These scopes can be chained. Like so::
+
+        # Returns a max of 5 records that have a name containing 'bob'
+        # ordered
+        Model.name_like('bob').ordered().limit(5)
+
+    """
+
+    def __init__(self, dict_or_callable=None, **kwargs):
+        if callable(dict_or_callable):
+            self.callable = dict_or_callable
+            self.name = self.callable.__name__
+        elif isinstance(dict_or_callable, dict):
+            kwargs.update(dict_or_callable)
+        elif dict_or_callable is not None:
+            raise Exception(
+                    "Invalid Scope parameter %s.  Must be dict or callable"
+                    % dict_or_callable)
+
+        if not hasattr(self, 'name'):
+            self.name = None
+
+        # Set when the attribute is fetched through __get__
+        self.model = None
+        self.finder_options = kwargs
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            self.model = owner
+            return self
+        else:
+            raise Exception("Scopes cannot be applied on an instance.")
+
+    def __call__(self, *args, **kwargs):
+        if hasattr(self, 'callable'):
+            return self.callable(self.model, *args, **kwargs)
+        else:
+            return self.model.scoped().apply_finder_options(
+                    self.finder_options)
+
---
+++ tests/scope_test.py
@@ -0,0 +1,108 @@
+import tests
+import unittest
+from nose.plugins.skip import SkipTest
+
+import pyperry
+from pyperry import errors
+from pyperry.scope import Scope
+
+import tests.fixtures.association_models
+
+class ScopeTestCase(unittest.TestCase):
+    pass
+
+class InitMethodTestCase(ScopeTestCase):
+
+    def test_takes_dict_or_callable(self):
+        """should accept a dict or callable as first argument"""
+        scope1 = Scope({ 'where': 'foo' })
+        self.assertEqual(scope1.finder_options, { 'where': 'foo' })
+
+        call = lambda(cls): cls.where('foo')
+        scope2 = Scope(call)
+        self.assertEqual(scope2.callable, call)
+
+    def test_accepts_keywords(self):
+        """should accept keywords and merge with dict"""
+        scope = Scope(where='bar', limit=2)
+        self.assertEqual(scope.finder_options, {'where': 'bar', 'limit': 2})
+
+        scope = Scope({ 'where': 'foo' }, limit=1)
+        self.assertEqual(scope.finder_options, {'where': 'foo', 'limit': 1})
+
+    def test_sets_name(self):
+        """should set `name` to None or __name__ of callable if present"""
+        scope = Scope()
+        self.assertEqual(scope.name, None)
+
+        @Scope
+        def scope2(cls): pass
+
+        self.assertEqual(scope2.name, 'scope2')
+
+    def test_sets_model(self):
+        """should set `model` to None"""
+        scope = Scope()
+        self.assertEqual(scope.model, None)
+
+    def test_errors_on_bad_argument(self):
+        """should raise exception on bad arguments"""
+        self.assertRaises(Exception, Scope, 'foo')
+        self.assertRaises(Exception, Scope, 1)
+        self.assertRaises(Exception, Scope, [])
+        self.assertRaises(Exception, Scope, tuple())
+
+class DescriptorTestCase(ScopeTestCase):
+
+    def setUp(self):
+        super(DescriptorTestCase, self).setUp()
+        self.scope = Scope()
+        class Test(object):
+            foo = self.scope
+        self.Test = Test
+
+    def test_sets_model_to_owner(self):
+        """should set `model` to owner when instance is None"""
+        self.assertEqual(self.Test.foo.model, self.Test)
+
+    def test_returns_self(self):
+        """should return self"""
+        self.assertEqual(self.Test.foo, self.scope)
+
+    def test_raises_when_called_from_instance(self):
+        """should raise exception when called from instance"""
+        test = self.Test()
+        self.assertRaises(Exception, getattr, test, 'foo')
+
+class CallableTestCase(ScopeTestCase):
+
+    def setUp(self):
+        super(CallableTestCase, self).setUp()
+
+        class Test(object):
+            @Scope
+            def foo(cls, *args, **kwargs):
+                self.last_call = (args, kwargs)
+                return 123
+
+        self.Test = Test
+
+    def test_delegates_callable(self):
+        """should delegate to `callable` when present"""
+        foo = self.Test.foo
+
+        self.assertEqual(foo(), 123)
+
+        foo(1, 2, 3, foo='bar')
+        self.assertEqual(((1, 2, 3), dict(foo='bar')), self.last_call)
+
+    def test_calls_finder_options(self):
+        """should use apply_finder_options to apply dicts"""
+        class Test(pyperry.base.Base):
+            foo = Scope(where='foo')
+
+        rel = Test.foo()
+
+        self.assertEqual(type(rel), pyperry.relation.Relation)
+        self.assertEqual(rel.query(), { 'where': ['foo'] })
+
